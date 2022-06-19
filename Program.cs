6bc3d567bdcbbf5970d2c62f8026fdde88b53ba9
000VIJAY <vijay893689@gmail.com>
2022-06-19 13:04:50 +0530

    Added Fibonacci series Problem

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
 ﻿Console.WriteLine("Welcome to logical programming problem");
+Console.WriteLine("Write the number whose fibonacci series wants to know  ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int numOne = 0;
+Console.WriteLine("num : " +numOne);
+int numTwo = 1;
+Console.WriteLine("num : "+numTwo);
+int numThree;
+for (int i = 2; i < num; i++)
+{
+    numThree = numOne + numTwo;
+        Console.WriteLine("num : " + numThree);
+    numOne = numTwo;
+    numTwo = numThree;
+}
+
