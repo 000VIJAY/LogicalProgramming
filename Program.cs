6756aa666340e5e56cf342701c9a5de4906aac43
000VIJAY <vijay893689@gmail.com>
2022-06-19 14:53:30 +0530

    added program for perfect number

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Welcome to logical programming problem");
+﻿Console.WriteLine("Write the number");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i < number; i++)  //i act as a divisor
+{
+    int remainder = number % i;
+   
+    if (remainder == 0)
+    {
+        sum = sum + i;
+        Console.WriteLine( "sum :" +sum );
+
+    }
+}
+    if (sum == number)
+    {
+        Console.WriteLine("number is perfect");
+    }
+    else
+    {
+        Console.WriteLine("number is not perfect");
+    }
