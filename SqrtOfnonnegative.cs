e19a10bd0d326bd0082b20752b3a92fcea658043
000VIJAY <vijay893689@gmail.com>
2022-06-20 21:45:09 +0530

    added Squre root of non negative function using newton method

---
+++ SqrtOfnonnegative.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgrammingProblem
+{
+    public class SqrtOfnonnegative
+    {
+        public static void Coupen()
+        {
+            Console.WriteLine("write number whose squre root wants to find");
+            double c =Convert.ToDouble(Console.ReadLine());
+            double t =c;
+            Console.WriteLine("value of t : " +t);
+            double x= c / t;
+            double epsilon = 1e-15;
+            while (Math.Abs(t - x) > epsilon * t )
+            {
+                t = (t+x)/2;
+            }
+            Console.WriteLine("The accuracy of desired value : " + t);
+
+        }
+        public static void Main(string[]strings)
+        {
+            SqrtOfnonnegative.Coupen();
+        }
+    }
+}
+
